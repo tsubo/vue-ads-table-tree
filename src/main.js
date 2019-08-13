b5a5f7edf98225e84922d5ce77e2697c1b2ea22a
tsubo <ktsubota@gmail.com>
2019-08-14 03:26:11 +0900

    add fontawesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.config.productionTip = false;
 
